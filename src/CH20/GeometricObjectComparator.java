0683143308de2f15473fe5379868089c0496169c
ThanhDat22 <dat.nguyen2284@gmail.com>
2024-02-21 14:52:34 -0600

    Adds TestComparator

---
+++ src/CH20/GeometricObjectComparator.java
@@ -0,0 +1,14 @@
+package CH20;
+
+public class GeometricObjectComparator implements Comparator<GeometricObject>, java.io.Serializable {
+    public int compare(GeometricObject ob1, GeometricObject ob2){
+        double area1 = ob1.getArea();
+        double area2 = ob2.getArea();
+
+        if(area1 < area2) {return -1;}
+        else if(area1 == area2){return 0;}
+        else {
+            return 1;
+        }
+    }
+}
