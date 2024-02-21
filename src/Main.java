884beb2a5b8599d475cd6f0c46ff72f35299016e
ThanhDat22 <dat.nguyen2284@gmail.com>
2024-02-21 01:37:25 -0600

    Set up

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> collection1 = new ArrayList<>();
+        collection1.add("New Yourk");
+
+        System.out.println(collection1);
+        System.out.println("Hello world!");
+    }
+}
